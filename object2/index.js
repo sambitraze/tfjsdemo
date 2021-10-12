9e6b53b1ca94392fc8b3681326ae9f276e3cd28b
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-10-12 22:13:33 +0530

    u

--- object2/index.js
+++ object2/index.js
@@ -1,8 +1,6 @@
 const videoElem = document.getElementById("videoElem");
 const canvasRef = document.getElementById("canvasRef");
-var script = document.createElement('script');
-script.src = 'https://rawgit.com/paulirish/memory-stats.js/master/bookmarklet.js';
-document.head.appendChild(script);
+const im = document.getElementById("im");
 
 const labelMap = {
     1: { name: 'Book', color: 'red' },
@@ -11,12 +9,11 @@ const labelMap = {
 
 // Define a drawing function
 drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx) => {
-    ctx.drawImage(videoElem, 0, 0, imgWidth, imgHeight);
     for (let i = 0; i <= boxes.length; i++) {
         if (boxes[i] && classes[i] && scores[i] > threshold) {
-            console.log("yes");
             const [y, x, height, width] = boxes[i]
             const text = classes[i]
+            console.log(labelMap[text]['name'] + ' - ' + Math.round(scores[i] * 100) / 100,);
             // Set styling
             ctx.strokeStyle = labelMap[text]['color']
             ctx.lineWidth = 2
@@ -40,51 +37,62 @@ window.onload = async function () {
         const MODEL_URL = './model/model.json';
         const model = await tf.loadGraphModel(MODEL_URL);
         detection = setInterval(async () => {
-            detect(model);
-        }, 100);
+            await detect(model);
+        }, 1000);
 
     }).catch((err) => {
         alert(err);
     });
 }
 
-const detect = async (net) => {    
+const detect = async (net) => {
     const videoWidth = videoElem.videoWidth;
     const videoHeight = videoElem.videoHeight;
     canvasRef.width = videoElem.videoWidth;
     canvasRef.height = videoElem.videoHeight;
 
     // 4. TODO - Make Detections
-    const img = tf.browser.fromPixels(videoElem);
-    const resized = tf.image.resizeBilinear(img, [640, 640]);
-    const casted = resized.cast("int32");
-    const expanded = casted.expandDims(0);
-    const obj = await net.executeAsync(expanded);
+    
+    const exp = tf.tidy(() => {
+        const img = tf.browser.fromPixels(videoElem);
+        const resized = tf.image.resizeBilinear(img, [640, 640]);
+        const casted = resized.cast("int32");
+        const expanded = casted.expandDims(0);
+        return expanded;
+    });
 
-    const boxes = await obj[4].array(); //
-    const classes = await obj[6].array(); //Classes  
-    const scores = await obj[0].array(); // 
-
+    const obj = await net.executeAsync(exp);
+    const boxes = await obj[2].array(); //
+    const classes = await obj[4].array(); //Classes  
+    const scores = await obj[7].array();
     // Draw mesh
     const ctx = canvasRef.getContext("2d");
 
     // 5. TODO - Update drawing utility
     // drawSomething(obj, ctx)
-    requestAnimationFrame(() => {
-        drawRect(
-            boxes[0],
-            classes[0],
-            scores[0],
-            0.5,
-            videoWidth,
-            videoHeight,
-            ctx
-        );
-    });
+    // requestAnimationFrame(() => {
+    // drawRect(
+    //     boxes[0],
+    //     classes[0],
+    //     scores[0],
+    //     0.8,
+    //     videoWidth,
+    //     videoHeight,
+    //     ctx
+    // );
+    // });
 
-    tf.dispose(img);
-    tf.dispose(resized);
-    tf.dispose(casted);
-    tf.dispose(expanded);
+    for (let i = 0; i <= boxes[0].length; i++) {
+        if (boxes[0][i] && classes[0][i] && scores[0][i] > 0.4) {
+            const text = classes[0][i]
+            console.log(labelMap[text]['name'] + ' - ' + Math.round(scores[0][i] * 100) / 100,);
+        }
+    }
+
+
+    // tf.dispose(img);
+    // tf.dispose(resized);
+    // tf.dispose(casted);
+    // tf.dispose(expanded);
     tf.dispose(obj);
 };
